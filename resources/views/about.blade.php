44c3fee5220db302e2e5ab06a775f25e74ca7308
Richard Hsieh <monaludao@gmail.com>
2018-08-14 22:32:45 +0200

    add about page fix-non-float navbar

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+@extends('main')
+
+@section('title', '首頁')
+
+@section('content')
+    <p>g0v 開放都市計畫專案</p>
+    <p>由許多貢獻者協作</p>
+    <p>歡迎加入協作討論</p>
+    <p>Links:</p>
+    <ul>
+        <li><a href="https://hackfoldr.urbancode.tw/" target="_blank">hackfolder</a></li>
+        <li><a href="https://www.facebook.com/groups/470451239772824/" target="_blank">facebook group</a></li>
+    </ul>
+
+@endsection
